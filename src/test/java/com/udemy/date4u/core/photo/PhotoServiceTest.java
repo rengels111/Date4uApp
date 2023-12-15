3a28252f089f24c36664bb624048c44fdea5ea7a
Raphael <info@engels-it.com>
2023-12-15 18:42:20 +0100

    FileSystemTest: with SpringBootTest and all Components autowired. (@MockBean, @Autowired)

--- src/test/java/com/udemy/date4u/core/photo/PhotoServiceTest.java
+++ src/test/java/com/udemy/date4u/core/photo/PhotoServiceTest.java
@@ -2,27 +2,23 @@ package com.udemy.date4u.core.photo;
 
 import com.udemy.date4u.core.FileSystem;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Base64;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
-// @SpringBootTest
-@ExtendWith(MockitoExtension.class)  // Mockito für Mock-Objekte.
+@SpringBootTest( properties = "spring.shell.interactive.enabled=false" )  // Spring Shell deaktivieren, damit Test
+// funktioniert.
+// @ExtendWith(MockitoExtension.class)  // Mockito für Mock-Objekte. Brauchen wir nun nicht mehr mit SpringBootTest.
 class PhotoServiceTest {
   private static final byte[] MINIMAL_JPG = Base64.getDecoder().decode(
      "/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAP////////////////////////////////////"
     +"//////////////////////////////////////////////////wgALCAABAAEBAREA/8QA"
-    +"FBABAAAAAAAAAAAAAAAAAAAAAP/aAAgBAQABPxA=" );    // https://git.io/J9GXr
+    +"FBABAAAAAAAAAAAAAAAAAAAAAP/aAAgBAQABPxA=" );
 
 //  Das brauchen wir nun nicht mehr, da Mockito verwendet wird.
 //  private static class DummyFileSystem extends FileSystem {
@@ -31,13 +27,17 @@ class PhotoServiceTest {
 //    @Override public void store( String filename, byte[] bytes ) { }
 //  }
 
-  @Mock(strictness = Mock.Strictness.LENIENT)  // Da wir nicht alle Methoden nutzen wollen, nehmen wir "lenient" hinzu.
+//  @Mock(strictness = Mock.Strictness.LENIENT)  // Da wir nicht alle Methoden nutzen wollen, nehmen wir "lenient" hinzu.
+//  FileSystem fs;
+//  @Spy
+//  AwtBicubicThumbnail thumbnail;
+//  @InjectMocks
+//  PhotoService photoService;  // im Hintergrund wird der parametrisierte Konstruktor mit dem Mock- und Spy-Objekt
+//  // übergeben.
+
+  @MockBean  // Erzeugt eine echte Bean aus dem SpringContext.
   FileSystem fs;
-  @Spy
-  AwtBicubicThumbnail thumbnail;
-  @InjectMocks
-  PhotoService photoService;  // im Hintergrund wird der parametrisierte Konstruktor mit dem Mock- und Spy-Objekt
-  // übergeben.
+  @Autowired PhotoService photoService;  // Nun kann ich den echten PhotoService autowiren.
 
   @Test
   void successful_photo_upload() {
@@ -51,8 +51,8 @@ class PhotoServiceTest {
     assertThat( imageName ).isNotEmpty();
     // Mockito-Methode verify: Verifiziere bitte, dass die Methode "store" der FileSystem-Klasse aufgerufen wurde:
     // times: verifiziere, dass store zweimal aufgerufen wurde:
-    verify(fs, times(2)).store(anyString(), any(byte[].class));
-    // Verifiziere, dass die Thumbnail-Methode mit einem byte[] aufgerufen wurde:
-    verify(thumbnail).thumbnail(any(byte[].class));
+//    verify(fs, times(2)).store(anyString(), any(byte[].class));
+//    // Verifiziere, dass die Thumbnail-Methode mit einem byte[] aufgerufen wurde:
+//    verify(thumbnail).thumbnail(any(byte[].class));
   }
 }
